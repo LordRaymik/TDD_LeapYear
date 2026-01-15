eade2a37f2dc58e8832f113aa87f5901fddfc11f
Lord-Raymik <silas.johnson@bsu.edu>
2026-01-15 11:52:25 -0500

    copied over files

---
+++ src/main/java/edu/bsu/cs/LeapYearCalculator.java
@@ -0,0 +1,14 @@
+package edu.bsu.cs;
+
+public class LeapYearCalculator {
+
+    public static boolean isLeapYear (int year) {
+        if (year % 4 == 0 && year % 100 != 0) {
+            return true;
+        } else if (year % 400 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ src/test/java/edu/bsu/cs/LeapYearCalculatorTest.java
@@ -0,0 +1,19 @@
+package edu.bsu.cs;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LeapYearCalculatorTest {
+
+    @Test
+    public void test2024 () {
+        boolean result = LeapYearCalculator.isLeapYear(2024);
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void test2000 () {
+        boolean result = LeapYearCalculator.isLeapYear(2000);
+        Assertions.assertTrue(result);
+    }
+}
